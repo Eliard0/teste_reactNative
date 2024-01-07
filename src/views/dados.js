c25e7f1bc5137842cb16c6971df6229d20de5333
Eliard0 <eliardosilva81@gmail.com>
2024-01-07 20:27:44 -0300

    feat mostrando os dados do banco na tela de dados

--- src/views/dados.js
+++ src/views/dados.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, Button, View, SafeAreaView, ScrollView, RefreshControl } from 'react-native';
 import db from '../services/db';
 
 const Dados = () => {
   const [dados, setDados] = useState([]);
+  const [recarregar, setRecarregar] = useState(false)
 
-  useEffect(()=>{
+  useEffect(() => {
+    buscarDados();
+  }, []);
+
+  const buscarDados = () => {
     db.transaction(tx => {
       tx.executeSql(
         'SELECT * FROM sensor;',
@@ -19,24 +24,63 @@ const Dados = () => {
         }
       );
     });
-  },[db])
+  }
+
+  const carregarDados = () => {
+    setRecarregar(true);
+    buscarDados();
+    setRecarregar(false);
+  };
+
+  const apagarDados = () => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'DELETE FROM sensor;',
+        [],
+        (_, result) => {
+          setDados(result.rows._array);
+        },
+        (_, error) => {
+          console.log('Error:', error);
+        }
+      );
+    });
+    setDados([]);
+  }
 
   if (dados.length === 0) {
     return (
-      <View style={styles.container}>
-        <Text>Não ha dados registrados no banco de dados</Text>
-      </View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text>Não ha dados registrados no banco de dados</Text>
+        </View>
+      </SafeAreaView>
     );
   }
 
   return (
-    <View style={styles.container}>
-      {dados.map(sensor => (
-        <Text key={sensor.id} style={styles.text}>
-          {sensor.nome_sensor}, {sensor.dado}
-        </Text>
-      ))}
-    </View>
+    <SafeAreaView>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={recarregar} onRefresh={carregarDados} />
+        }
+      >
+        <View style={styles.container}>
+          {dados.map(sensor => (
+            <Text key={sensor.id} style={styles.text}>
+              Nome do sensor: {sensor.nome_sensor}
+              {'\n'}
+              Dado: {sensor.dado}
+            </Text>
+          ))}
+
+          <Button
+            title='apagar todos os dados'
+            onPress={apagarDados}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -44,15 +88,13 @@ export default Dados
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   },
 
   text: {
-    fontSize: 25,
+    fontSize: 15,
     fontWeight: 'bold',
   },
-
-
 });
